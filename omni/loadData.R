47b2a3ddc79a92da90476a001c5a4f0e3333d8b6
new account <chandork@flores.sen.cwi.nl>
2015-10-06 17:29:12 +0200

    Initial commit 1) sde-rnn python virtual env 2) Omni data loader R script

---
+++ omni/loadData.R
@@ -0,0 +1,17 @@
+#! /usr/bin/R
+args <- commandArgs(trailingOnly = TRUE)
+year <- args[1]
+prefix <- "omni2_"
+system(paste("wget ftp://spdf.gsfc.nasa.gov/pub/data/omni/low_res_omni/",
+             prefix, year, ".dat", sep = ""))
+system(paste("sed -i 's/ \\{1,\\}/,/g' ", prefix, year, ".dat", sep = ""))
+
+
+df <- read.csv(paste(prefix, year, ".dat", sep = ""), 
+               header = FALSE, stringsAsFactors = FALSE, 
+               colClasses = rep("numeric",55), 
+               na.strings = c("99", "999.9", 
+                              "9999.", "9.999", "99.99", 
+                              "9999", "999999.99", 
+                              "99999.99", "9999999."))
+str(df)
