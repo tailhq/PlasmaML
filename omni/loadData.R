08ef13237c158055655f9d1b38a9ddc2d928c436
Mandar Chandorkar <mandar2812@gmail.com>
2015-10-12 17:39:59 +0200

    Mod to load data r script access monetdb table

--- omni/loadData.R
+++ omni/loadData.R
@@ -15,4 +15,24 @@ df <- read.csv(paste(prefix, year, ".dat", sep = ""),
                               "9999.", "9.999", "99.99", 
                               "9999", "999999.99", 
                               "99999.99", "9999999."))
-str(df)
+
+library(MonetDB.R)
+bat.file <- monetdb.server.setup(paste(getwd(), "monetdb", sep="/"),
+                                 monetdb.program.path = "~/monetdb/bin/", 
+                                 dbname = "omni", dbport = 50000)
+correct.pid <- monetdb.server.start(bat.file)
+
+conn <- dbConnect(MonetDB.R(), "monetdb://localhost:50000/omni")
+
+monetdb.read.csv(conn, paste(prefix, year, ".dat", sep = ""), "omniHourly", 
+                 nrows=8760, header=FALSE,
+                 locked=FALSE, 
+                 na.strings=c("99", "999.9", 
+                              "9999.", "9.999", "99.99", 
+                              "9999", "999999.99", 
+                              "99999.99", "9999999."), 
+                 nrow.check=500, delim=",", 
+                 newline = "\\n", quote = "\"", 
+                 create=TRUE)
+
+monetdb.server.stop(correct.pid, wait = TRUE)
