2ddbf52564d0899641ba611b4b8b815bc6f00c56
Mandar Chandorkar <mandar2812@gmail.com>
2015-10-20 18:22:23 +0200

    RNN not training well

--- omni/rnnCompare.R
+++ omni/rnnCompare.R
@@ -1,17 +1,24 @@
 #! /usr/bin/Rscript
 
 library(RSNNS)
+library(caret)
 
-trainFeatures <- df[1:5000,c(17,22,25,26,27)]
-trainTargets <- df[1:5000,41]
+trainFeatures <- preProcess(as.matrix(df[1:5000,c(17,22,25,26,27)]))
+trainTargets <- preProcess(as.matrix(df[1:5000,41]))
 
-testFeatures <- df[5001:8760,c(17,22,25,26,27)]
-testTargets <- df[5001:8760,41]
+testFeatures <- preProcess(as.matrix(df[5001:8760,c(17,22,25,26,27)]))
+testTargets <- preProcess(as.matrix(df[5001:8760,41]))
 
-modelEL <- jordan(x = trainFeatures, y = trainTargets,
+modelEL <- jordan(x = trainFeatures$data, y = trainTargets,
                  inputsTest = testFeatures, learnFunc = "JE_BP_Momentum",
-                 targetsTest = testTargets, size = 4)
+                 targetsTest = testTargets, size = 10, maxit = 500)
+
+
+plot(x = as.double(1:length(testTargets)), y = testTargets, type = 'l')
+
+lines(as.double(1:length(testTargets)),modelEL$fittedTestValues,col="blue")
+
 
 plotRegressionError(testTargets, modelEL$fittedTestValues)
 
-hist(modelEL$fittedTestValues - testTargets, col="lightblue")
+hist(modelEL$fittedTestValues - testTargets, col="lightblue", breaks=100)
