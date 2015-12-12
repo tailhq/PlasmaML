5e7fc13022bb819f87b0674a2f394b8bf2bb4104
mandar2812 <mandar2812@gmail.com>
2015-12-12 14:46:24 +0100

    Added heatmaps

--- rnn-omni-explorer/about.md
+++ rnn-omni-explorer/about.md
@@ -1,7 +1,11 @@
 ### Omni 2 Data Explorer/Recurrent Neural Net Test Bed
 
-This interactive app enables the user to generate exploratory plots of the omni data and train RNN models on it based on the following quantities.
+This interactive app enables the user to generate exploratory plots of the omni data and train Recurrent Neural Network models on it based on the following quantities.
 
 * **Dst** - Kyoto Disturbance Storm Time index (nT)
 * **Vs** - Solar Wind Velocity (km/s)
 * **Bz** - IMF Bz (nT)
+* **S** - Sunsopt Number
+* **AE** - AE index
+* **T** - Proton Temperature (K)
+* **P** - Flow Pressure (nPa)
--- rnn-omni-explorer/app.R
+++ rnn-omni-explorer/app.R
@@ -3,6 +3,8 @@ library(shiny)
 library(markdown)
 library(DT)
 library(RSNNS)
+library(ggplot2)
+library(reshape2)
 prefix <- "omni2_"
 year <- 2006
 
@@ -39,19 +41,64 @@ layout <- navbarPage("Omni Explorer",
                                 )
                               )
                      ),
-                     tabPanel("Explore",
-                              sidebarLayout(
-                                sidebarPanel(
-                                  selectInput("year1", "Year",
-                                              choices = c("2006" = "2006",
-                                                          "2007" = "2007"),
-                                              selected = "2006")
-                                  
+                     navbarMenu("Explore", 
+                                tabPanel("Scatter Plot",
+                                         sidebarLayout(
+                                           sidebarPanel(
+                                             selectInput("year1", "Year",
+                                                         choices = c("2006" = "2006",
+                                                                     "2007" = "2007"),
+                                                         selected = "2006"),
+                                             selectInput("xaxis", "X Axis",
+                                                         choices = c("Solar Wind Speed" = "Vsw",
+                                                                     "Interplanetary Magnetic Field Bz" = "Bz",
+                                                                     "Proton Temperature" = "T",
+                                                                     "Flow Pressure" = "P",
+                                                                     "Sunspot Number" = "S"),
+                                                         selected = "Vsw"),
+                                             selectInput("yaxis", "Y Axis",
+                                                         choices = c("Dst" = "Dst",
+                                                                     "AE" = "AE",
+                                                                     "Proton Temperature" = "T",
+                                                                     "Flow Pressure" = "P"),
+                                                         selected = "Dst")
+                                             
+                                           ),
+                                           mainPanel(
+                                             plotOutput("eplot")
+                                           )
+                                         )
                                 ),
-                                mainPanel(
-                                  plotOutput("eplot")
+                                tabPanel("Correlation Heatmap",
+                                         sidebarLayout(
+                                           sidebarPanel(
+                                             selectInput("year2", "Year",
+                                                         choices = c("2006" = "2006",
+                                                                     "2007" = "2007"),
+                                                         selected = "2006"),
+                                             selectInput("ctype", "Correlation Type",
+                                                         choices = c("Pearson" = "pearson",
+                                                                     "Spearman" = "spearman", 
+                                                                     "Kendall" = "kendall"),
+                                                         selected = "2006"),
+                                             checkboxGroupInput("cvars", "Chosen Variables",
+                                                                choices = c("Solar Wind Speed" = "Vsw",
+                                                                            "Interplanetary Magnetic Field Bz" = "Bz",
+                                                                            "Proton Temperature" = "T",
+                                                                            "Flow Pressure" = "P",
+                                                                            "Sunspot Number" = "S",
+                                                                            "Dst" = "Dst",
+                                                                            "AE" = "AE"
+                                                                ),
+                                                                selected = c("Vsw", "Bz", "Dst")
+                                             )
+                                           ),
+                                           mainPanel(
+                                             plotOutput("cplot")
+                                           )
+                                         )
                                 )
-                              )
+                                
                      ),
                      navbarMenu("More",
                                 tabPanel("Table",
@@ -77,7 +124,7 @@ server <- function(input, output, session) {
     df <- read.csv(paste(prefix, input$year, ".dat", sep = ""), 
                    header = FALSE, stringsAsFactors = FALSE, 
                    colClasses = rep("numeric",55), 
-                   na.strings = c("99", "999.9", 
+                   na.strings = c("99", "999.9",
                                   "9999.", "9.999", "99.99", 
                                   "9999", "999999.99", 
                                   "99999.99", "9999999."))
@@ -132,19 +179,53 @@ server <- function(input, output, session) {
     df <- read.csv(paste(prefix, input$year1, ".dat", sep = ""), 
                    header = FALSE, stringsAsFactors = FALSE, 
                    colClasses = rep("numeric",55), 
-                   na.strings = c("99", "999.9", 
+                   na.strings = c("99", "999.9",  "999", "99",
                                   "9999.", "9.999", "99.99", 
                                   "9999", "999999.99", 
                                   "99999.99", "9999999."))
     
-    processedDF <- df[,c(17, 22, 25, 41)]
+    processedDF <- df[,c(17, 25, 41, 39, 40, 42, 23, 29)]
     
-    colnames(processedDF) <- c("Bz", "SigmaBz", "Vsw", "Dst")
-    plot(processedDF$Vsw, processedDF$Dst)
+    colnames(processedDF) <- c("Bz", "Vsw", "Dst", "Kp", "S", "AE", "T", "P")
+    #plot(processedDF$Vsw, processedDF$Dst, col = processedDF$Kp)
+    finalDF <- processedDF[, c(input$xaxis, input$yaxis)]
+    colnames(finalDF) <- c("input", "output")
+    ggplot(finalDF, aes(x = input, y = output)) + geom_point()
+    
+  })
+  
+  output$cplot <- renderPlot({
+    df <- read.csv(paste(prefix, input$year2, ".dat", sep = ""), 
+                   header = FALSE, stringsAsFactors = FALSE, 
+                   colClasses = rep("numeric",55), 
+                   na.strings = c("99", "999.9",  "999", "99",
+                                  "9999.", "9.999", "99.99", 
+                                  "9999", "999999.99", 
+                                  "99999.99", "9999999."))
+    
+    processedDF <- df[,c(17, 25, 41, 39, 40, 42, 23, 29)]
+    
+    colnames(processedDF) <- c("Bz", "Vsw", "Dst", "Kp", "S", "AE", "T", "P")
+    cormat <- cor(processedDF[, input$cvars], use="complete.obs", method = input$ctype)
+    melted_cormat <- melt(cormat)
+    ggplot(data = melted_cormat, aes(x=Var1, y=Var2, fill=value)) + 
+      geom_tile()
+    
   })
   
   output$table <- DT::renderDataTable({
-    DT::datatable(cars)
+    df <- read.csv(paste(prefix, "2006", ".dat", sep = ""), 
+                   header = FALSE, stringsAsFactors = FALSE, 
+                   colClasses = rep("numeric",55), 
+                   na.strings = c("99", "999.9",  "999", "99",
+                                  "9999.", "9.999", "99.99", 
+                                  "9999", "999999.99", 
+                                  "99999.99", "9999999."))
+    
+    processedDF <- df[,c(1,2,3,17, 25, 41, 39, 40, 42, 23, 29)]
+    
+    colnames(processedDF) <- c("Year","Day","Hour","Bz","Vsw", "Dst", "Kp", "S", "AE", "T", "P")
+    DT::datatable(processedDF[,c("Year","Day","Hour","Vsw", "Bz", "Dst")])
   })
 }
 
