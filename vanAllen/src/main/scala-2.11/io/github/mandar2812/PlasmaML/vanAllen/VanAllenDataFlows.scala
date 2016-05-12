210e9120e80026b6e2e1fb97c59392a66946602c
mandar2812 <mandar2812@gmail.com>
2016-05-12 14:06:22 +0200

    Van Allen: Added function to extract files from a data category into a Spark RDD

--- vanAllen/src/main/scala-2.11/io/github/mandar2812/PlasmaML/vanAllen/VanAllenDataFlows.scala
+++ vanAllen/src/main/scala-2.11/io/github/mandar2812/PlasmaML/vanAllen/VanAllenDataFlows.scala
@@ -5,10 +5,12 @@ import java.text.SimpleDateFormat
 import java.util.{Calendar, Date, GregorianCalendar}
 
 import scala.collection.mutable.{MutableList => ML}
-import io.github.mandar2812.dynaml.pipes.DataPipe
+import io.github.mandar2812.dynaml.pipes.{DataPipe, DynaMLPipe}
 import io.github.mandar2812.dynaml.utils
 import org.apache.log4j.Logger
+import org.apache.spark.rdd.RDD
 import org.apache.spark.{SparkConf, SparkContext}
+import org.json4s
 import org.json4s._
 import org.json4s.jackson.JsonMethods._
 
@@ -22,6 +24,8 @@ import org.json4s.jackson.JsonMethods._
   */
 object VanAllenDataFlows {
 
+  private val greg = new GregorianCalendar()
+
   val logger = Logger.getLogger(this.getClass)
 
   var dataRoot: String = "data/"
@@ -32,11 +36,34 @@ object VanAllenDataFlows {
 
   val cleanRegex = """\}(\"\w)""".r
 
+  val timeColumns: Seq[String] = Seq("Year", "Month", "Day", "Hour", "Minute", "Second")
+
+  val positionReferenceFrame: String = "LShell"
+
+  val positionColumns: Seq[String] = positionReferenceFrame.capitalize match {
+    case "LSHELL" => Seq("LShELL", "LAT", "LON")
+    case "GSE" => Seq("GSEX", "GSEY", "GSEZ")
+    case "GSM" => Seq("GSMX", "GSEM", "GSEM")
+    case "GEI" => Seq("GSMX", "GSEM", "GSEM")
+    case _ => Seq("LShELL", "LAT", "LON") //Defaults to L-Shell coordinate frame
+  }
+
   val sparkHost = "local[*]"
 
-  private val greg = new GregorianCalendar()
+  val sc = new SparkContext(
+    new SparkConf().setMaster(sparkHost)
+      .setAppName("Van Allen Data Models")
+      .set("spark.executor.memory", "4g"))
 
-  val sc = new SparkContext(new SparkConf().setMaster(sparkHost).setAppName("Van Allen Data Models"))
+  implicit class RDDOps[T](rdd: RDD[T]) {
+
+    def partitionBy(f: T => Boolean): (RDD[T], RDD[T]) = {
+      val passes = rdd.filter(f)
+      val fails = rdd.filter(e => !f(e)) // Spark doesn't have filterNot
+      (passes, fails)
+    }
+  }
+
 
   /**
     * Returns a DynaML [[DataPipe]] that takes a [[Stream]] of lines
@@ -66,6 +93,30 @@ object VanAllenDataFlows {
           (content, jsonParsed)
       }
     })
+
+
+  def stripRDDHeader(keepHeader: Boolean = false) = DataPipe((rdd: RDD[String]) => {
+    val (content, header) = rdd.partitionBy(_.head != headerLead)
+
+    val processedHeader = header.map(line =>
+      line.takeRight(line.length-2)
+        .trim
+        .replace(headerEndStr, "")).collect()
+
+    keepHeader match {
+      case false =>
+        (content, None)
+      case true =>
+        val jsonParsed = try {
+          Some(parse(cleanRegex.replaceAllIn(processedHeader.mkString(""), """\}\,$1"""), false))
+        } catch {
+          case e: Exception => None
+        }
+
+        (content, jsonParsed)
+    }
+  })
+
 
   def fileToStreamOption = DataPipe((relativePath: String) => {
     Files.exists(Paths.get(dataRoot+relativePath)) match {
@@ -113,18 +164,18 @@ object VanAllenDataFlows {
     * within a specified date range
     *
     * */
-  def extractData(startDate: String,
-                  endDate: String,
-                  category: String = VanAllenData.dataCategories.keys.head,
-                  probes: Seq[String] = VanAllenData.probes,
-                  columns: Seq[String] = Seq()) = {
+  def extractDataStream(startDate: String,
+                        endDate: String,
+                        category: String = VanAllenData.dataCategories.keys.head,
+                        probes: Seq[String] = VanAllenData.probes,
+                        columns: Seq[String] = Seq()) = {
 
 
     var columnData: Option[JValue] = None
 
     val dates = dateRange(startDate, endDate)
 
-    probes.map(probe => {
+    val dataStream = probes.map(probe => {
       val data = dates.map(date => {
         greg.setTime(date)
         val doy = greg.get(Calendar.DAY_OF_YEAR)
@@ -145,6 +196,67 @@ object VanAllenDataFlows {
         }
       }).reduceLeft[Stream[String]](_++_)
       (probe, data)
+    }).toMap
+
+    (dataStream, columnData)
+  }
+
+  /**
+    * Extract data from a particular category
+    * within a specified date range into a Spark RDD.
+    *
+    * */
+  def extractDataRDD(startDate: String,
+                     endDate: String,
+                     category: String = VanAllenData.dataCategories.keys.head,
+                     probes: Seq[String] = VanAllenData.probes,
+                     columns: Seq[String] = Seq()) = {
+
+
+    var columnData: Option[JValue] = None
+
+    val dates = dateRange(startDate, endDate)
+
+    probes.map(probe => {
+      val data = dates.map(date => {
+        greg.setTime(date)
+        val doy = greg.get(Calendar.DAY_OF_YEAR)
+        val year = greg.get(Calendar.YEAR)
+
+        val fileContent = fileToRDDOption.run(category + "_" + probe + "_" + year + "_" + doy + ".txt")
+        fileContent match {
+          case Some(cont) =>
+            val (buff, json) = stripRDDHeader(true).run(cont)
+            if (columnData.isEmpty & json.isDefined) {
+              columnData = json
+            }
+            buff
+
+          case None =>
+            logger.info("No file found: " + category + " probe " + probe + " for " + year + "/" + doy)
+            sc.emptyRDD[String]
+        }
+      }).reduceLeft[RDD[String]]((a, b) => a.union(b))
+
+      val columnsSelected = columnData match {
+        case Some(columnInfo) =>
+          (timeColumns ++ columns).map(columnStr => {
+            compact(render(columnInfo\columnStr\"START_COLUMN")).toInt
+          }).toList
+        case None =>
+          List(1,2,3,4,5,6)
+      }
+
+      val processPartition = DataPipe((s: Iterator[String]) => s.toStream) >
+        DynaMLPipe.trimLines >
+        DynaMLPipe.replaceWhiteSpaces >
+        DynaMLPipe.extractTrainingFeatures(
+          columnsSelected,
+          columnsSelected.map(c => (c, " ")).toMap
+        ) > DataPipe((s: Stream[String]) => s.toIterator) run _
+
+
+      (probe, data.mapPartitions(processPartition))
     }).toMap
   }
 }
