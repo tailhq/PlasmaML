1509882e210a82bdb1d5807069a8906504bec45f
mandar2812 <mandar2812@gmail.com>
2016-05-27 01:16:00 +0200

    Added neural net for CRRES

--- vanAllen/src/main/scala-2.11/io/github/mandar2812/PlasmaML/vanAllen/CRRESTest.scala
+++ vanAllen/src/main/scala-2.11/io/github/mandar2812/PlasmaML/vanAllen/CRRESTest.scala
@@ -2,8 +2,10 @@ package io.github.mandar2812.PlasmaML.vanAllen
 
 import breeze.linalg.{DenseMatrix, DenseVector}
 import io.github.mandar2812.PlasmaML.cdf.CDFUtils
+import io.github.mandar2812.dynaml.evaluation.RegressionMetrics
 import io.github.mandar2812.dynaml.kernels.{CovarianceFunction, DiracKernel, RBFKernel}
 import io.github.mandar2812.dynaml.models.gp.GPRegression
+import io.github.mandar2812.dynaml.models.neuralnets.{FFNeuralGraph, FeedForwardNetwork}
 import io.github.mandar2812.dynaml.pipes.{DataPipe, DynaMLPipe, GPRegressionPipe}
 
 /**
@@ -69,7 +71,49 @@ object CRRESTest {
     finalPipe("/var/Datasets/space-weather/crres/crres_h0_mea_19910201_v01.cdf")
   }
 
-  def apply(num_train: Int, num_test: Int) = {
+  def apply(hidden: Int, acts: List[String], nCounts: List[Int],
+            num_train: Int, num_test: Int, stepSize: Double,
+            maxIt: Int, alpha: Double, regularization: Double,
+            mini: Double) = {
+    val modelTrainTest =
+      (trainTest: ((Stream[(DenseVector[Double], Double)],
+        Stream[(DenseVector[Double], Double)]),
+        (DenseVector[Double], DenseVector[Double]))) => {
 
+        val gr = FFNeuralGraph(trainTest._1._1.head._1.length, 1, hidden,
+          acts, nCounts)
+
+        val transform = DataPipe((d: Stream[(DenseVector[Double], Double)]) =>
+          d.map(el => (el._1, DenseVector(el._2))))
+
+        val model = new FeedForwardNetwork[Stream[(DenseVector[Double], Double)]](trainTest._1._1, gr, transform)
+
+        model.setLearningRate(stepSize)
+          .setMaxIterations(maxIt)
+          .setBatchFraction(mini)
+          .setMomentum(alpha)
+          .setRegParam(regularization)
+          .learn()
+
+        val res = model.test(trainTest._1._2)
+        val scoresAndLabelsPipe =
+          DataPipe(
+            (res: Seq[(DenseVector[Double], DenseVector[Double])]) =>
+              res.map(i => (i._1(0), i._2(0))).toList) > DataPipe((list: List[(Double, Double)]) =>
+            list.map{l => (l._1*trainTest._2._2(-1) + trainTest._2._1(-1),
+              l._2*trainTest._2._2(-1) + trainTest._2._1(-1))})
+
+        val scoresAndLabels = scoresAndLabelsPipe.run(res)
+
+        val metrics = new RegressionMetrics(scoresAndLabels,
+          scoresAndLabels.length)
+
+        metrics.print()
+        metrics.generatePlots()
+      }
+
+    val finalPipe = preProcess(num_train, num_test) > DataPipe(modelTrainTest)
+
+    finalPipe("/var/Datasets/space-weather/crres/crres_h0_mea_19910201_v01.cdf")
   }
 }
