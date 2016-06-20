e13a00770a558a0acc5401c8b80bd2e96b067666
mandar2812 <mandar2812@gmail.com>
2016-06-20 22:29:23 +0200

    Mods to Omni MSA Dst script

--- omni/scripts/waveletOmniMSA.scala
+++ omni/scripts/waveletOmniMSA.scala
@@ -8,4 +8,4 @@ OmniWaveletModels(2e-1, 0.0, 0.0, 30, 1.0, useWaveletBasis = true)
 // Increase the number of features to boost 6 hr predictions
 OmniWaveletModels.orderFeat = 4
 
-OmniWaveletModels(2e-1, 0.0, 0.2, 40, 1.0, useWaveletBasis = true)
+OmniWaveletModels(4e-1, 0.01, 0.4, 20, 1.0, useWaveletBasis = true)
