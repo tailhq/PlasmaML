d5c54360d8d76affd078d609242d56f941658eda
mandar2812 <mandar2812@gmail.com>
2016-08-17 11:33:08 +0200

    Cleaning up code in MO GP script

--- omni/scripts/omniMOGP.scala
+++ omni/scripts/omniMOGP.scala
@@ -4,13 +4,13 @@ import io.github.mandar2812.dynaml.kernels._
 
 val linearK = new PolynomialKernel(1, 0.0)
 val fbmK = new DiracKernel(0.1)
-val rbfKernel = new CauchyKernel(1.5)
+val cauKernel = new CauchyKernel(1.5)
 
 fbmK.blocked_hyper_parameters = fbmK.hyper_parameters
 linearK.blocked_hyper_parameters = linearK.hyper_parameters
 
-val d = new DiracKernel(0.92)
-//d.blocked_hyper_parameters = List("noiseLevel")
+val d = new DiracKernel(0.32)
+d.blocked_hyper_parameters = List("noiseLevel")
 
 val n = new CoRegCauchyKernel(3.5)
 n.blocked_hyper_parameters = n.hyper_parameters
@@ -19,7 +19,7 @@ val k = new CoRegLaplaceKernel(5.0)
 k.blocked_hyper_parameters = k.hyper_parameters
 val k1 = new CoRegDiracKernel
 
-val kernel: CompositeCovariance[(DenseVector[Double], Int)] = (linearK :* k) + (rbfKernel :* n)
+val kernel: CompositeCovariance[(DenseVector[Double], Int)] = (linearK :* k) + (cauKernel :* n)
 val noise: CompositeCovariance[(DenseVector[Double], Int)] = d :* k1
 
 OmniWaveletModels.exogenousInputs = List(16,24)
