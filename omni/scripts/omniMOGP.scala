b151a849c97eb4c6f50f9cab8dbd8ee113bd419a
mandar2812 <mandar2812@gmail.com>
2016-08-17 14:36:46 +0200

    Mods to MO GP script

--- omni/scripts/omniMOGP.scala
+++ omni/scripts/omniMOGP.scala
@@ -3,23 +3,27 @@ import io.github.mandar2812.PlasmaML.omni.{DstMOGPExperiment, OmniWaveletModels}
 import io.github.mandar2812.dynaml.kernels._
 
 val linearK = new PolynomialKernel(1, 0.0)
-val fbmK = new DiracKernel(0.1)
 val cauKernel = new CauchyKernel(1.5)
 
-fbmK.blocked_hyper_parameters = fbmK.hyper_parameters
 linearK.blocked_hyper_parameters = linearK.hyper_parameters
 
 val d = new DiracKernel(0.32)
 d.blocked_hyper_parameters = List("noiseLevel")
 
-val n = new CoRegCauchyKernel(3.5)
-n.blocked_hyper_parameters = n.hyper_parameters
+val coRegCauchyMatrix = new CoRegCauchyKernel(3.5)
+coRegCauchyMatrix.blocked_hyper_parameters = coRegCauchyMatrix.hyper_parameters
 
-val k = new CoRegLaplaceKernel(5.0)
-k.blocked_hyper_parameters = k.hyper_parameters
+val coRegLaplaceMatrix = new CoRegLaplaceKernel(5.0)
+coRegLaplaceMatrix.blocked_hyper_parameters = coRegLaplaceMatrix.hyper_parameters
 val k1 = new CoRegDiracKernel
 
-val kernel: CompositeCovariance[(DenseVector[Double], Int)] = (linearK :* k) + (cauKernel :* n)
+val waveletF = (x: Double) => math.cos(1.75*x)*math.exp(-1*x*x/2.0)
+
+val waveletKernel = new WaveletKernel(waveletF)(3.5)
+
+
+val kernel: CompositeCovariance[(DenseVector[Double], Int)] = (linearK :* coRegLaplaceMatrix) + (cauKernel :* coRegCauchyMatrix) + (waveletKernel :* coRegCauchyMatrix)
+
 val noise: CompositeCovariance[(DenseVector[Double], Int)] = d :* k1
 
 OmniWaveletModels.exogenousInputs = List(16,24)
