8e6dd91e98eac48f451a5b1022b5bb92d24c9583
mandar2812 <mandar2812@gmail.com>
2016-11-04 19:18:30 +0100

    OMNI OSA: Added R script for model selection/comparison plots

---
+++ omni/scripts/gpOmniOrder.R
@@ -0,0 +1,45 @@
+library(plyr)
+library(ggplot2)
+library(gridExtra)
+library(reshape2)
+library(latex2exp)
+
+
+df <- read.csv("Nov_4_2016_alt_7_1_OmniARXStormsRes.csv", 
+               header = FALSE, stringsAsFactors = TRUE, 
+               col.names = c("eventID","stormCat","order", "modelSize",
+                             "rmse", "corr", "deltaDstMin", "DstMin",
+                             "deltaT"))
+
+
+df1 <- read.csv("Nov_4_2016_alt_6_1_OmniARXStormsRes.csv", 
+               header = FALSE, stringsAsFactors = TRUE, 
+               col.names = c("eventID","stormCat","order", "modelSize",
+                             "rmse", "corr", "deltaDstMin", "DstMin",
+                             "deltaT"))
+
+df2 <- read.csv("Nov_4_2016_alt_5_1_OmniARXStormsRes.csv", 
+               header = FALSE, stringsAsFactors = TRUE, 
+               col.names = c("eventID","stormCat","order", "modelSize",
+                             "rmse", "corr", "deltaDstMin", "DstMin",
+                             "deltaT"))
+
+df3 <- read.csv("Nov_4_2016_alt_4_1_OmniARXStormsRes.csv", 
+               header = FALSE, stringsAsFactors = TRUE, 
+               col.names = c("eventID","stormCat","order", "modelSize",
+                             "rmse", "corr", "deltaDstMin", "DstMin",
+                             "deltaT"))
+
+df4 <- read.csv("Nov_4_2016_alt_3_1_OmniARXStormsRes.csv", 
+                header = FALSE, stringsAsFactors = TRUE, 
+                col.names = c("eventID","stormCat","order", "modelSize",
+                              "rmse", "corr", "deltaDstMin", "DstMin",
+                              "deltaT"))
+
+
+dfFinal <- rbind(df, df1, df2, df3, df4)
+
+qplot(data = dfFinal, x = factor(order), y = corr,
+      geom = "boxplot", 
+      xlab = "Model Order", ylab = "Model Corr Coefficient") + 
+  theme_gray(base_size = 14)
