5f5b137ac5be2274c88fa70e657941b6e58f82fc
mandar2812 <mandar2812@gmail.com>
2017-03-31 14:16:00 +0200

    Bare bones implementation of Radial Diffusion solver
     - [WIP]: The processing of diffusion profiles into Neural Stack
    Parameters

--- core/src/main/scala-2.11/io/github/mandar2812/PlasmaML/RadialDiffusionSolver.scala
+++ core/src/main/scala-2.11/io/github/mandar2812/PlasmaML/RadialDiffusionSolver.scala
@@ -1,14 +1,33 @@
 package io.github.mandar2812.PlasmaML
 
+import breeze.linalg.{DenseMatrix, DenseVector}
+import io.github.mandar2812.dynaml.models.neuralnets._
+import io.github.mandar2812.dynaml.pipes.DataPipe3
+
 /**
-  * Created by mandar on 30/03/2017.
+  * @author mandar2812 date 30/03/2017.
   */
-abstract class RadialDiffusionSolver(
+class RadialDiffusionSolver(
   lShellLimits: (Double, Double), timeLimits: (Double, Double),
-  nL: Int, nT: Int) {
+  nL: Int, nT: Int, processDiffusionParams: DataPipe3[
+  DenseMatrix[Double], DenseMatrix[Double], DenseMatrix[Double],
+  Seq[(DenseMatrix[Double], DenseVector[Double])]]) {
 
   val (deltaL, deltaT) = ((lShellLimits._2 - lShellLimits._1)/nL, (timeLimits._2 - timeLimits._1)/nT)
 
+  val stackFactory = NeuralStackFactory(
+    (1 to nT).map(_ => new Vec2VecLayerFactory(VectorLinear)(nL+1, nL+1)):_*
+  )
 
+  val computeStackParameters = processDiffusionParams
+
+  def getComputationStack = computeStackParameters > stackFactory
+
+  def solve(
+    lossProfile: DenseMatrix[Double],
+    diffusionProfile: DenseMatrix[Double],
+    boundaryFlux: DenseMatrix[Double])(
+    f0: DenseVector[Double]): Seq[DenseVector[Double]] =
+    getComputationStack(lossProfile, diffusionProfile, boundaryFlux).forwardPropagate(f0)
 
 }
