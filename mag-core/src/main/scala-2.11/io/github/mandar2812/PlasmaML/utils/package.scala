b9fb93a94870d906a828f4663c1f5f7e60355b6e
mandar2812 <mandar2812@gmail.com>
2017-08-15 12:31:23 +0200

    Added Fourier and Polynomial basis generators

--- mag-core/src/main/scala-2.11/io/github/mandar2812/PlasmaML/utils/package.scala
+++ mag-core/src/main/scala-2.11/io/github/mandar2812/PlasmaML/utils/package.scala
@@ -1,10 +1,37 @@
 package io.github.mandar2812.PlasmaML
 
+import breeze.linalg._
+import io.github.mandar2812.dynaml.pipes._
+
 /**
   * Miscellaneous utilities for the PlasmaML software distribution.
   *
   * @author mandar2812 date 16/05/2017.
   * */
 package object utils {
+
+
+  /**
+    * Generates a fourier series feature mapping.
+    * */
+  object FourierSeriesGenerator extends MetaPipe21[Double, Int, Double, DenseVector[Double]] {
+
+    override def run(omega: Double, components: Int): DataPipe[Double, DenseVector[Double]] = {
+
+      DataPipe((x: Double) => DenseVector((0 to components).map(i => math.sin(i*omega*x)).toArray))
+    }
+
+  }
+
+  /**
+    * Generates a polynomial feature mapping upto a specified degree.
+    * */
+  object PolynomialSeriesGenerator extends MetaPipe[Int, Double, DenseVector[Double]] {
+
+    override def run(degree: Int): DataPipe[Double, DenseVector[Double]] = {
+
+      DataPipe((x: Double) => DenseVector((0 to degree).map(d => math.pow(x, d)).toArray))
+    }
+  }
 
 }
