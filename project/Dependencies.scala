37420fd5744e837dd8f89865623d814b647e429f
mandar2812 <mandar2812@gmail.com>
2018-01-23 23:54:38 +0100

    Updated TF-Scala dependency

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -76,8 +76,8 @@ object Dependencies {
     .map(_.exclude("org.platanios", "tensorflow-data_2.11"))
 
   val tf =
-    if(packagedTFFlag) "org.platanios" % "tensorflow_2.11" % "0.1.0-SNAPSHOT" classifier tensorflow_classifier
-    else "org.platanios" % "tensorflow_2.11" % "0.1.0-SNAPSHOT"
+    if(packagedTFFlag) "org.platanios" % "tensorflow_2.11" % "0.1.0" classifier tensorflow_classifier
+    else "org.platanios" % "tensorflow_2.11" % "0.1.0"
 
   val tf_examples = "org.platanios" % "tensorflow-data_2.11" % "0.1.0-SNAPSHOT"
 
