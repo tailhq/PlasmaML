30ab670c583980beb1421a2234e79cbb2eafc862
mandar2812 <mandar2812@gmail.com>
2018-01-26 16:59:40 +0100

    Correction to DynaML dependency

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -20,7 +20,7 @@ object Dependencies {
   val dataDirectory = settingKey[File]("The directory holding the data files for running example scripts")
 
   val (dynamlGroupID, dynamlArtifact, dynaMLVersion) =
-    if(status == "local") ("io.github.mandar2812.DynaML", "dynaml_2.11", "v1.5.2-beta.2")
+    if(status == "local") ("io.github.mandar2812", "dynaml_2.11", "v1.5.2-beta.2")
     else if(status == "dev") ("com.github.transcendent-ai-labs.DynaML", "dynaml_2.11", "master-SNAPSHOT")
     else ("com.github.transcendent-ai-labs.DynaML", "dynaml_2.11", "v1.5.2-beta.2")
 
