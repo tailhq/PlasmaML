3ca9303ba209318547e4d895bbca8984b4bccf4e
mandar2812 <mandar2812@gmail.com>
2018-02-06 23:45:21 +0100

    helios:minor script corrections

--- helios/scripts/mdi_goes_test.sh
+++ helios/scripts/mdi_goes_test.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
+
 yeartest=2001
 limit=2006
 mdiscript="./helios/scripts/goes_extreme_events.sc"
 
 while [ "$yeartest" -lt "$limit" ]; do
-./target/universal/stage/bin/plasmaml $mdiscript $yeartest
+exec ./target/universal/stage/bin/plasmaml $mdiscript $yeartest
 let yeartest+=1
 done
+
+!#
