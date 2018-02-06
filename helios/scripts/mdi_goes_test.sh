83370089191085d24dfb30acc4e9657151b388fe
mandar2812 <mandar2812@gmail.com>
2018-02-06 23:25:36 +0100

    helios:[WIP] mods to mdi script

--- helios/scripts/mdi_goes_test.sh
+++ helios/scripts/mdi_goes_test.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 yeartest=2001
 limit=2006
+mdiscript="./helios/scripts/goes_extreme_events.sc"
 
 while [ "$yeartest" -lt "$limit" ]; do
-./target/universal/stage/bin/plasmaml "./helios/scripts/goes_extreme_events.sc" "$yeartest"
+./target/universal/stage/bin/plasmaml $(mdiscript) $(yeartest)
 let yeartest+=1
 done
