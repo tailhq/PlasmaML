dc8b62311152a2b6102fd6f33d303a865ee02363
mandar2812 <mandar2812@gmail.com>
2018-02-06 23:16:11 +0100

    helios: More fixes to mdi test

--- helios/scripts/mdi_goes_test.sh
+++ helios/scripts/mdi_goes_test.sh
@@ -3,6 +3,6 @@ yeartest=2001
 limit=2006
 
 while [ "$yeartest" -lt "$limit" ]; do
-./target/universal/stage/bin/plasmaml $"yeartest"
+./target/universal/stage/bin/plasmaml "./helios/scripts/goes_extreme_events.sc" $"yeartest"
 let yeartest+=1
 done
