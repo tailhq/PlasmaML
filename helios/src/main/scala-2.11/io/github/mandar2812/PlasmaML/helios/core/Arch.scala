6667b814dda9b386eadd553ba44ba2019bbf8131
mandar2812 <mandar2812@gmail.com>
2018-02-09 17:04:45 +0100

    helios: Added helper function in `Arch` object
     - Convolutional building block

--- helios/src/main/scala-2.11/io/github/mandar2812/PlasmaML/helios/core/Arch.scala
+++ helios/src/main/scala-2.11/io/github/mandar2812/PlasmaML/helios/core/Arch.scala
@@ -10,26 +10,36 @@ import org.platanios.tensorflow.api.ops.NN.SamePadding
   * */
 object Arch {
 
+
+  /**
+    * Constructs a convolutional layer activated by a ReLU, with
+    * an option of appending a dropout layer.
+    *
+    * */
+  def conv2d_unit(
+    shape: Shape, stride: (Int, Int) = (1, 1),
+    relu_param: Float = 0.1f, dropout: Boolean = true,
+    keep_prob: Float = 0.6f)(i: Int) =
+    if(dropout) {
+      tf.learn.Conv2D("Conv2D_"+i, shape, 1, 1, SamePadding) >>
+        tf.learn.AddBias(name = "Bias_"+i) >>
+        tf.learn.ReLU("ReLU_"+i, 0.1f) >>
+        tf.learn.Dropout("Dropout_"+i, 0.6f)
+    } else {
+      tf.learn.Conv2D("Conv2D_"+i, shape, 1, 1, SamePadding) >>
+        tf.learn.AddBias(name = "Bias_"+i) >>
+        tf.learn.ReLU("ReLU_"+i, 0.1f)
+    }
+
   /**
     * CNN architecture for GOES XRay flux (short wavelength)
     * */
   val cnn_goes_v1 = {
     tf.learn.Cast("Input/Cast", FLOAT32) >>
-      tf.learn.Conv2D("Conv2D_0", Shape(2, 2, 4, 64), 1, 1, SamePadding) >>
-      tf.learn.AddBias(name = "Bias_0") >>
-      tf.learn.ReLU("ReLU_0", 0.1f) >>
-      tf.learn.Dropout("Dropout_0", 0.6f) >>
-      tf.learn.Conv2D("Conv2D_1", Shape(2, 2, 64, 32), 2, 2, SamePadding) >>
-      tf.learn.AddBias(name = "Bias_1") >>
-      tf.learn.ReLU("ReLU_1", 0.1f) >>
-      tf.learn.Dropout("Dropout_1", 0.6f) >>
-      tf.learn.Conv2D("Conv2D_2", Shape(2, 2, 32, 16), 4, 4, SamePadding) >>
-      tf.learn.AddBias(name = "Bias_2") >>
-      tf.learn.ReLU("ReLU_2", 0.1f) >>
-      tf.learn.Dropout("Dropout_2", 0.6f) >>
-      tf.learn.Conv2D("Conv2D_3", Shape(2, 2, 16, 8), 8, 8, SamePadding) >>
-      tf.learn.AddBias(name = "Bias_3") >>
-      tf.learn.ReLU("ReLU_3", 0.1f) >>
+      conv2d_unit(Shape(2, 2, 4, 64), (1, 1))(0) >>
+      conv2d_unit(Shape(2, 2, 64, 32), (2, 2))(1) >>
+      conv2d_unit(Shape(2, 2, 32, 16), (4, 4))(2) >>
+      conv2d_unit(Shape(2, 2, 16, 8), (8, 8), dropout = false)(3) >>
       tf.learn.MaxPool("MaxPool_3", Seq(1, 2, 2, 1), 1, 1, SamePadding) >>
       tf.learn.Flatten("Flatten_3") >>
       tf.learn.Linear("FC_Layer_4", 128) >>
