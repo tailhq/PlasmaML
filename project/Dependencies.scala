49eeb0c28f4d04d898e8ae3e71ff268a7630946d
mandar2812 <mandar2812@gmail.com>
2018-05-28 12:48:11 +0200

    helios: OMNI causal time lag
     - Perform arbitrary image pre-processing before creating data set
    
    Signed-off-by: mandar2812 <mandar2812@gmail.com>

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -15,7 +15,7 @@ object Dependencies {
   val packagedTFFlag: Boolean = true
 
   //Set to dev, if pulling DynaML master SNAPSHOT
-  val status = "dev"
+  val status = "prod"
 
   val dataDirectory = settingKey[File]("The directory holding the data files for running example scripts")
 
