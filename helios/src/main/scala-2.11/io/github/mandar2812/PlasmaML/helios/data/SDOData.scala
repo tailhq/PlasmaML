057eabfbf87c31e92acd4b3087f63fc7f32b08a1
mandar2812 <mandar2812@gmail.com>
2018-06-04 22:50:04 +0200

    helios:  - Improvements to sdo download method  - Added bulk download script for sdo data Signed-off-by: mandar2812 <mandar2812@gmail.com>
    Signed-off-by: mandar2812 <mandar2812@gmail.com>

--- helios/src/main/scala-2.11/io/github/mandar2812/PlasmaML/helios/data/SDOData.scala
+++ helios/src/main/scala-2.11/io/github/mandar2812/PlasmaML/helios/data/SDOData.scala
@@ -112,7 +112,7 @@ object SDOLoader {
       mkdir! download_path
     }
 
-    println("Downloading image manifest from the SOHO Archive for: "+date+"\nDownload Path: "+download_path)
+    println("Downloading image manifest from the SDO Archive for: "+date+"\nDownload Path: "+download_path)
 
     download_batch(download_path)(fetch_urls(path)(instrument, size)(year, month, day))
   }
