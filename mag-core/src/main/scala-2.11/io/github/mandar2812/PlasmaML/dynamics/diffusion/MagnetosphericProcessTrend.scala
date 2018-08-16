8c34951fac747297d9bef1095dfa8ee513fdf151
mandar2812 <mandar2812@gmail.com>
2018-08-16 23:58:12 +0200

    [mag-core]:   -- Added `MagParamBasis` class
    Signed-off-by: mandar2812 <mandar2812@gmail.com>

--- mag-core/src/main/scala-2.11/io/github/mandar2812/PlasmaML/dynamics/diffusion/MagnetosphericProcessTrend.scala
+++ mag-core/src/main/scala-2.11/io/github/mandar2812/PlasmaML/dynamics/diffusion/MagnetosphericProcessTrend.scala
@@ -1,7 +1,10 @@
 package io.github.mandar2812.PlasmaML.dynamics.diffusion
 
+import breeze.linalg.DenseVector
+import io.github.mandar2812.dynaml.{analysis, utils}
 import io.github.mandar2812.PlasmaML.utils.MagConfigEncoding
-import io.github.mandar2812.dynaml.pipes.{DataPipe, Encoder, MetaPipe}
+import io.github.mandar2812.dynaml.analysis.DifferentiableMap
+import io.github.mandar2812.dynaml.pipes._
 
 /**
   * Specifies a Brautingham &amp; Albert type
@@ -103,3 +106,36 @@ object MagTrend {
 
 
 class SimpleMagTrend(prefix: String) extends MagTrend(DataPipe((_: Double) => 0d), prefix)
+
+
+abstract class MagParamBasis extends
+  Basis[Double] with
+  DifferentiableMap[Double, DenseVector[Double], DenseVector[Double]]
+
+
+object MagParamBasis {
+
+  def apply(func: Double => DenseVector[Double], grad_func: Double => DenseVector[Double]): MagParamBasis =
+    new MagParamBasis{
+
+      override protected val f: Double => DenseVector[Double] = func
+
+      override def J(x: Double): DenseVector[Double] = grad_func(x)
+    }
+
+  val polynomial_basis: Int => MagParamBasis = n => apply(
+    analysis.PolynomialBasisGenerator(n).run _,
+    x => DenseVector.tabulate(n + 1)(i => i * math.pow(x, i - 1))
+  )
+
+  val chebyshev_basis: Int => MagParamBasis = n => apply(
+    analysis.ChebyshevBasisGenerator(n, 1).run _,
+    x => DenseVector(Array(0d) ++ Array.tabulate(n)(i => if(i+1 > 0) (i+1)*utils.chebyshev(i, x, kind = 2) else 0d))
+  )
+
+  val hermite_basis: Int => MagParamBasis = n => apply(
+    analysis.HermiteBasisGenerator(n).run _,
+    x => DenseVector(Array(0d) ++ (1 to n).toArray.map(i => i*utils.hermite(i - 1, x)))
+  )
+
+}
