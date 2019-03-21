3babc27924c09ecf6c4d851f49300de8b3946196
mandar2812 <mandar2812@gmail.com>
2019-03-21 15:34:06 +0100

    minor mod

--- clean-build-caches.sh
+++ clean-build-caches.sh
@@ -4,5 +4,5 @@ echo 'Cleaning local Ivy cache'
 rm -rf ~/.ivy/*
 
 echo 'Cleaning local coursier caches (if any)'
-rm ~/.coursier/*
-rm ~/Library/Caches/Coursier/*
+rm -rf ~/.coursier/*
+rm -rf ~/Library/Caches/Coursier/*
