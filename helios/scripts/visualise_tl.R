e8b8dd3a6ed82309770f4f2ef0ad81559ce05c74
mandar2812 <mandar2812@gmail.com>
2019-04-05 12:04:45 +0200

    minor mods

--- helios/scripts/visualise_tl.R
+++ helios/scripts/visualise_tl.R
@@ -15,7 +15,7 @@ scatter_df <- read.csv(file, header = FALSE)
 colnames(scatter_df) <- c("Prediction", "Actual", "TimeLag")
 
 
-ggplot(scatter_df, aes(x=Actual, y=Prediction)) +
+ggplot(scatter_df, aes(x=Prediction, y=Actual)) +
   theme_gray(base_size = 20) + 
   geom_point(alpha=1/3) +    
   geom_smooth(method=lm) +
