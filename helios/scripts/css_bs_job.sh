c8d61e24a11343497f167e7b2bc83d507c0efeee
mandar2812 <mandar2812@gmail.com>
2019-05-23 14:58:23 +0200

    [helios]: Deleting old file

--- helios/scripts/css_bs_job.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-id=${1:-csss_exp1}
-
-dir=${2}
-
-root=${3:-$HOME/tmp}
-
-./target/universal/stage/bin/plasmaml ./helios/scripts/csss_job.sc --csss_job_id $id --exp_dir $dir --root_dir $root
