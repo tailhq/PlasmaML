1c228cdd223f76915d1a53150008e885b6d7c589
mandar2812 <mandar2812@gmail.com>
2019-08-05 15:27:56 +0200

    [mag-core]: Added method to plot surrogate model.

--- mag-core/src/main/scala/io/github/mandar2812/PlasmaML/dynamics/diffusion/RDExperiment.scala
+++ mag-core/src/main/scala/io/github/mandar2812/PlasmaML/dynamics/diffusion/RDExperiment.scala
@@ -3,6 +3,7 @@ package io.github.mandar2812.PlasmaML.dynamics.diffusion
 import breeze.linalg.{DenseMatrix, DenseVector}
 import breeze.numerics.Bessel
 import breeze.stats.distributions._
+import io.github.mandar2812.dynaml.graphics.plot3d
 import io.github.mandar2812.dynaml.graphics.charts.Highcharts.{
   histogram,
   hold,
@@ -31,6 +32,8 @@ import ammonite.ops._
 import org.apache.log4j.Logger
 import io.github.mandar2812.dynaml.optimization.GloballyOptimizable
 import io.github.mandar2812.dynaml.probability.mcmc.AdaptiveHyperParameterMCMC
+import io.github.mandar2812.dynaml.evaluation.RegressionMetrics
+
 
 /**
   * <h3>Radial Diffusion Experiments</h3>
@@ -644,6 +647,59 @@ object RDExperiment {
 
     logger.info("Done writing results for experiment")
     resultsPath
+  }
+
+  def plot_surrogate(exp_result: RDExperiment.Result[SGRadialDiffusionModel]) = {
+
+    val colocation_points = RDExperiment.readColocation(
+      exp_result.results_dir / "colocation_points.csv"
+    )
+  
+    val (lShellVec, timeVec) =
+      RadialDiffusion.buildStencil(lShellLimits, nL, timeLimits, nT)
+  
+    val solution_data = timeVec
+      .zip(exp_result.solution.map(_.toArray.toSeq).map(lShellVec.zip(_)))
+      .flatMap(c => c._2.map(d => ((d._1, c._1), d._2)))
+      .toStream
+  
+    val solution_data_features = solution_data.map(_._1)
+  
+    val solution_targets = solution_data.map(_._2)
+  
+    val (params, psi) = exp_result.model.getParams(gt)
+  
+    val phi_sol = solution_data_features.map(exp_result.model.basis(_))
+  
+    val mean = phi_sol.map(p => {
+      val features = DenseVector.vertcat(
+        DenseVector(1d),
+        exp_result.model.phi * p,
+        psi * p
+      )
+      features.t * params
+    })
+  
+    val surrogate_preds =
+      solution_data
+        .zip(
+          mean.map(x => x * exp_result.model.psd_std + exp_result.model.psd_mean)
+        )
+        .map(c => (c._1._1, c._1._2, c._2))
+  
+    val metrics = new RegressionMetrics(
+      surrogate_preds.map(p => (p._3, p._2)).toList,
+      surrogate_preds.length
+    )
+  
+    (
+      surrogate_preds,
+      metrics,
+      plot3d.draw(surrogate_preds.map(p => (p._1, p._2))),
+      plot3d.draw(surrogate_preds.map(p => (p._1, p._3))),
+      plot3d.draw(surrogate_preds.map(p => (p._1, p._2 - p._3)))
+    )
+  
   }
 
   def writeResults(
