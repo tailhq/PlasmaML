d6fe1504642b84b014c3763a496733b645a95d50
mandar2812 <mandar2812@gmail.com>
2019-09-10 16:38:22 +0200

    [helios]: Added CV runner script

---
+++ helios/scripts/csss_cv_runner.sh
@@ -0,0 +1,22 @@
+#! /bin/bash
+
+filepath=${1:-"data/sw_cv_dtlr.txt"}
+
+id=${2:-dtlr_cv_sw}
+
+count=0
+
+nl=$(sed -n '$=' $filepath)
+
+echo "Solar Wind Cross Validation: $nl fold."
+echo "Experiment Id: $id"
+
+while read tr; do
+    echo "Running experiment for Carrington rotation $tr"
+    ./target/universal/stage/bin/plasmaml ./helios/scripts/csss_job.sc --csss_job_id $id --test_rotation $tr
+    if [ $? -eq 0 ]; then
+        count=$((count + 1))
+    fi
+done <$filepath
+
+echo "Successfully ran $count out of $nl folds."
